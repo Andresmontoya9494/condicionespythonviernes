9f1ccb2f7abc76ad5ad3a44154ba84d7fb0fccb5
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-08-18 10:29:00 -0500

    NT3 se inicia condicionales

---
+++ condicionales2.py
@@ -0,0 +1,8 @@
+temperatura =int(input("Digita la tempratura:"))
+gradosAlcohol = int(input("Digita grados de alcohol"))
+
+
+if temperatura>50 or gradosAlcohol>5:
+    print("Alarma")
+else:
+        print("normal")
---
+++ condiciones.py
@@ -0,0 +1,7 @@
+edadUsuario=int(input("Digita tu edad"))
+
+if(edadUsuario>=18):
+    print("Bienvenido a la disco")
+else:
+    print("eres menor de edad, pa la casa")
+    print("oeee pa la casa...")
